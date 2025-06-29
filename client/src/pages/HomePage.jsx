f4eb23ee079299e810a3ff6c927b4cde8129b4c3
rshdgvn <rasheedgavinesponga@gmail.com>
2025-06-30 04:44:18 +0800

    feat: created sidebar

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
+import SideBar from "../components/SideBar";
+import ChatContainer from "../components/ChatContainer";
+import RightSideBar from "../components/RightSideBar";
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+  const [selectedUser, setSelectedUser] = useState(false);
 
-export default HomePage
+  return (
+    <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
+      <div
+        className={`blackdrop-blur-xl border-2 bordery-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative
+        ${
+          selectedUser
+            ? "md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]"
+            : "md:grid-cols-2"
+        }`}
+      >
+        <SideBar
+          selectedUser={selectedUser}
+          setSelectedUser={setSelectedUser}
+        />
+        <ChatContainer
+          selectedUser={selectedUser}
+          setSelectedUser={setSelectedUser}
+        />
+        <RightSideBar
+          selectedUser={selectedUser}
+          setSelectedUser={setSelectedUser}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
